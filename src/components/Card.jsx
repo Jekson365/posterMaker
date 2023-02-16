ad2412be32c04010387d0458f1b9ed4ebcef5ea3
unknown <jemali.ergemlidze.1@btu.edu.ge>
2023-02-16 16:22:06 +0400

    cc

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 export const Card = (props) => {
-    const {image,name,genre,produced,studio} = props
-    return (
-        <>
-            <div className="box">
-                <div className="image">
-                    <img src={image} />
-                </div>
-                <div className="data">
-                    <div className='title'>{name}</div>
-                    <div className="p"><span>genre:</span>{genre}</div>
-                    <div className='studios'><span>studios:</span>{studio}</div>
-                    <div className="produce"><span>produced by:</span>{produced}</div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const { image, name, genre, produced, studio } = props.data;
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  return (
+    <>
+      <div className="box">
+        <div className="image">
+          <img src={image} />
+        </div>
+        <div className="data">
+          <div className="title">{name}</div>
+          <div className="p">
+            <span>genre:</span>
+            {genre}
+          </div>
+          <div className="studios">
+            <span>studios:</span>
+            {studio}
+          </div>
+          <div className="produce">
+            <span>produced by:</span>
+            {produced}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
